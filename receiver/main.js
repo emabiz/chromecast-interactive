e9595c70293adb15c3329c9e24b4d3788cf2ecdd
emabiz <emabiz76@gmail.com>
2023-03-15 17:34:09 +0100

    m

--- receiver/main.js
+++ receiver/main.js
@@ -9,7 +9,7 @@ window.onload = function() {
     });
   });
 
-  infoMsg('Receiver version 16');
+  infoMsg('Receiver version 17');
   infoMsg('UserAgent: '+window.navigator.userAgent );
 
   const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
