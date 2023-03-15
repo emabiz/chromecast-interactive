a17afebaacb2d2d231f3e9514aeafb1d1c57ad9e
emabiz <emabiz76@gmail.com>
2023-03-15 12:28:05 +0100

    m

--- receiver/main.js
+++ receiver/main.js
@@ -1,29 +1,39 @@
-/**
- * Main JavaScript for handling Chromecast interactions.
- */
 
 window.onload = function() {
-  cast.receiver.logger.setLevelValue(0);
+
+  const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
+
+  const context = cast.framework.CastReceiverContext.getInstance();
+
+  context.addEventListener(cast.framework.system.EventType.READY, () => {
+    if (!castDebugLogger.debugOverlayElement_) {
+        // Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
+        castDebugLogger.setEnabled(true);
+    }
+  });
+
+
+  // cast.receiver.logger.setLevelValue(0);
   window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
-  console.log('Starting Receiver Manager');
+  castDebugLogger.log('ema','Starting Receiver Manager');
 
   castReceiverManager.onReady = function(event) {
-    console.log('Received Ready event: ' + JSON.stringify(event.data));
+    castDebugLogger.log('ema','Received Ready event: ' + JSON.stringify(event.data));
     window.castReceiverManager.setApplicationState('chromecast is ready...');
   };
 
   castReceiverManager.onSenderConnected = function(event) {
-    console.log('Received Sender Connected event: ' + event.senderId);
+    castDebugLogger.log('ema','Received Sender Connected event: ' + event.senderId);
   };
 
   castReceiverManager.onSenderDisconnected = function(event) {
-    console.log('Received Sender Disconnected event: ' + event.senderId);
+    castDebugLogger.log('ema','Received Sender Disconnected event: ' + event.senderId);
   };
 
   window.messageBus =window.castReceiverManager.getCastMessageBus('urn:x-cast:com.emabiz.chromecast-interactive', cast.receiver.CastMessageBus.MessageType.JSON);
 
   window.messageBus.onMessage = function(event) {
-      console.log('Message [' + event.senderId + ']: ' + event.data);
+    castDebugLogger.log('ema','Message [' + event.senderId + ']: ' + event.data);
       switch(event.data.op){
           case 'play':
               video.play();
@@ -35,7 +45,7 @@ window.onload = function() {
 
   // Initialize the CastReceiverManager with an application status message.
   window.castReceiverManager.start({statusText: 'Application is starting'});
-  console.log('Receiver Manager started');
+  castDebugLogger.log('ema','Receiver Manager started');
 
 
   function infoMsg(msg) {
